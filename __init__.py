03a0d2d4b08762f02b99c4af8c43f4867e21b684
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2022-09-20 15:04:04 -0400

    commit10

--- __init__.py
+++ __init__.py
@@ -1,26 +1,16 @@
 import logging
-import json
-from jira import JIRA
+
 import azure.functions as func
+
+from calculator import highest_price_function
+
+import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    username="amit.894@gmail.com"
-    token="NotMyBitCoin"
-    options = {
-        'server': 'https://amit894.atlassian.net/'}
-    jira = JIRA(
-            basic_auth=(username,token),options=options)
 
-    jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
-                                  assignee={'name': 'amitraj'})
-    return func.HttpResponse(
-        json.dumps({
-            'method': req.method,
-            'url': req.url,
-            'headers': dict(req.headers),
-            'params': dict(req.params),
-            'get_body': req.get_body().decode()
-        })
-    )
+    max_prices = highest_price_function()
+    max_prices_json = json.dumps(max_prices)
+
+    return func.HttpResponse(max_prices_json, mimetype='text/json')
